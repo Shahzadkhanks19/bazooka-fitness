d79284882add9989065a153cd732b2ff45cc0502
Shahzad Khan <joykhan78600@gmail.com>
2026-08-31 23:22:16 +0530

    fix: improve faq mobile shell

--- components/faq/faq-shared.ts
+++ components/faq/faq-shared.ts
@@ -1,4 +1,4 @@
-export const shell = "mx-auto w-[min(calc(100%-40px),1180px)] md:w-[min(calc(100%-64px),1180px)]";
+export const shell = "mx-auto w-[min(calc(100%-32px),1180px)] sm:w-[min(calc(100%-40px),1180px)] md:w-[min(calc(100%-64px),1180px)]";
 
 export const reveal = {
   initial: { opacity: 0, y: 22 },
